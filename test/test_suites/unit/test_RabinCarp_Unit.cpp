fbbfaf72356f5881fd846a584529d05a3fce4196
maksn <maksim.nazaroff.main@gmail.com>
2025-02-12 20:01:29 +0300

    Add more Rabin-Carp util unit tests

--- test/test_suites/unit/test_RabinCarp_Unit.cpp
+++ test/test_suites/unit/test_RabinCarp_Unit.cpp
@@ -25,3 +25,40 @@ TEST_CASE("Test computeHash", "[unit][rabin-carp]") {
         REQUIRE(computeHash(text.substr(0, 10), PRIME, BASE) == computeHash(text.substr(14, 10), PRIME, BASE));
     }
 }
+
+TEST_CASE("Test computePowers", "[unit][rabin-carp]") {
+    const uint64_t PRIME = 1000000007;
+    const uint64_t BASE = 256;
+
+    SECTION("Empty String") {
+        size_t stringLength = 0;
+        std::vector<uint64_t> result = precomputePowers(stringLength, PRIME, BASE);
+        REQUIRE(result.empty());
+    }
+    
+    SECTION("Single Power") {
+        // verifies function correctness for strings of length 1
+        size_t stringLength = 1;
+        std::vector<uint64_t> result = precomputePowers(stringLength, PRIME, BASE);
+        REQUIRE(result.size() == 1);
+        REQUIRE(result[0] == 1);
+    }
+    
+    SECTION("Multiple Powers") {
+        size_t stringLength = 4;
+        std::vector<uint64_t> result = precomputePowers(stringLength, PRIME, BASE);
+        std::vector<uint64_t> expected = {1, BASE % PRIME, (BASE * BASE) % PRIME, (BASE * BASE * BASE) % PRIME};
+        REQUIRE(result == expected);
+    }
+    
+    SECTION("Large String") {
+        // verifies function correctness in cases where modulo starts to apply repeatedly during power calculation
+        size_t stringLength = 20;
+        std::vector<uint64_t> result = precomputePowers(stringLength, PRIME, BASE);
+        std::vector<uint64_t> expected(stringLength, 1);
+        for (size_t i = 1; i < stringLength; ++i) {
+            expected[i] = (expected[i - 1] * BASE) % PRIME;
+        }
+        REQUIRE(result == expected);
+    }
+}
