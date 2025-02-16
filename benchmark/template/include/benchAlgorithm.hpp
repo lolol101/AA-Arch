560310e77c0eafc70d6623bb6e815130e05e0240
maksn <maksim.nazaroff.main@gmail.com>
2025-02-16 21:02:24 +0300

    Add some benchmark method descriptions; prevent second action start on push w/ open PR

--- benchmark/template/include/benchAlgorithm.hpp
+++ benchmark/template/include/benchAlgorithm.hpp
@@ -7,12 +7,13 @@ using AlgoFuncType1 = std::function<std::vector<size_t>(const std::string &, con
 using AlgoFuncType2 = std::function<std::vector<std::vector<size_t>>(const std::string &, const std::set<std::string> &)>;
 
 namespace benchmark {
-
+/// @brief Generates random text and pattern from alphabet and calls findString(); used for performance tests.
 void runSinglePatternAlgo(AlgoFuncType1 findString, 
                             const std::string& alphabet,
                             unsigned textSize,
                             unsigned patternSize);
 
+/// @brief Generates random text and set of patterns from alphabet and calls findAllStrings(); used for performance tests.
 void runMultiPatternAlgo(AlgoFuncType2 findAllStrings, 
                             const std::string& alphabet,
                             unsigned textSize,
--- benchmark/template/include/benchmark.hpp
+++ benchmark/template/include/benchmark.hpp
@@ -12,8 +12,6 @@ namespace benchmark {
 /// @brief Loads config file and exits programm in case of error
 json loadConfig(const std::string& filename);
 
-void runTemplateTimeBenchmark(benchmark::State& state, BenchFuncType1 runSinglePatternAlgo, const std::string algoName);
-
 // Functions measure the time according to the scenario in config.json
 void benchmarkTimeAhoCorasik(benchmark::State& state);
 void benchmarkTimeKnuthMorrisPratt(benchmark::State& state);
